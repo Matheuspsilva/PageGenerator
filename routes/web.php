f3bcd403817674b4264e9dfe766de21b8c40364a
Matheus <matheuspsilva29@gmail.com>
2023-03-25 15:25:15 -0300

    ajuste rotas e auth

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,15 @@ use App\Http\Controllers\QrCodeController;
 |
 */
 
+Auth::routes();
+
 Route::get('/generate', [QrCodeController::class, 'index']);
 Route::post('/qrcode', [QrCodeController::class, 'generate'])->name('qrcode');
-Route::get('/{slug}', [QrCodeController::class, 'showPage']);
+Route::get('page/{slug}', [QrCodeController::class, 'showPage']);
 
-Auth::routes();
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
